901c635cf7d20679f35bda4653333a6b827b5bdb
Mark Dittmer <markdittmer@google.com>
2017-04-10 15:29:29 -0400

    Add WebKit clone-and-select() manual test
    Includes bug fix: LocalGitIDLFileDAO `find` invocation shouldn't match
    against complete path, just repository path.

---
+++ test/node/webKitIDLFileDAO-manual.js
@@ -0,0 +1,26 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+'use strict';
+
+var githubBaseURL = 'https://github.com/WebKit/webkit';
+var config = {
+  description: 'WebKit IDL file DAO manual test',
+  repositoryURL: 'https://github.com/WebKit/webkit.git',
+  localRepositoryPath: require('path').resolve(__dirname, '../data/WebKit/git'),
+  sparsePath: 'Source',
+  findExcludePatterns: ['*/testing/*', '*/test/*'],
+};
+config.idlFileContentsFactory = function(path, contents) {
+  // Classes are injected by integrationLocalGitTest.
+  return config.IDLFileContents.create({
+    metadata: config.GithubIDLFile.create({
+      repository: this.respositoryURL,
+      githubBaseURL: githubBaseURL,
+      revision: this.commit,
+      path: path,
+    }),
+    contents: contents,
+  });
+};
+global.manualLocalGitTest(config);
