bbf5a81a04fb296379e48b13117142230a7fda8d
Michael Cheung <michaelcheung@google.com>
2017-06-05 13:18:37 -0400

    Adding manual tests which parses browser IDLs

---
+++ test/node/blinkParseIDLFileDAO-manual.js
@@ -0,0 +1,27 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+'use strict';
+
+var gitilesBaseURL = 'https://chromium.googlesource.com/chromium/src/+';
+var config = {
+  description: 'Blink IDL file DAO Parser manual test',
+  repositoryURL: 'https://chromium.googlesource.com/chromium/src.git',
+  localRepositoryPath: require('path').resolve(__dirname, '../data/blink/git'),
+  sparsePath: 'third_party/WebKit/Source',
+  findExcludePatterns: ['*/testing/*', '*/bindings/tests/*', '*/mojo/*'],
+  extension: 'idl',
+  parser: 'Blink'
+};
+config.idlFileContentsFactory = function(path, contents) {
+  return config.IDLFileContents.create({
+    metadata: config.GitilesIDLFile.create({
+      repository: this.repositoryURL,
+      gitilesBaseURL: gitilesBaseURL,
+      revision: this.commit,
+      path: path,
+    }),
+    contents: contents,
+  });
+};
+global.parseIDLFileDAOTest(config);
---
+++ test/node/webKitParseIDLFileDAO-manual.js
@@ -0,0 +1,27 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+'use strict';
+
+var githubBaseURL = 'https://github.com/WebKit/webkit';
+var config = {
+  description: 'WebKit IDL file DAO manual test',
+  repositoryURL: 'https://github.com/WebKit/webkit.git',
+  localRepositoryPath: require('path').resolve(__dirname, '../data/WebKit/git'),
+  sparsePath: 'Source/WebCore',
+  findExcludePatterns: ['*/testing/*', '*/test/*'],
+  extension: 'idl',
+  parser: 'WebKitParser',
+};
+config.idlFileContentsFactory = function(path, contents) {
+  return config.IDLFileContents.create({
+    metadata: config.GithubIDLFile.create({
+      repository: this.repositoryURL,
+      githubBaseURL: githubBaseURL,
+      revision: this.commit,
+      path: path,
+    }),
+    contents: contents,
+  });
+};
+global.parseIDLFileDAOTest(config);
