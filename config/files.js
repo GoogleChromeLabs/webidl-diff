3af686c733371ea2de70eaa4607a8a4940cefba0
Michael Cheung <michaelcheung@google.com>
2017-06-20 15:48:29 -0400

    Adding URLExtractor class

--- config/files.js
+++ config/files.js
@@ -104,5 +104,8 @@
     `lib${sep}org${sep}chromium${sep}webidl${sep}DAOOperation.js`,
     `lib${sep}org${sep}chromium${sep}webidl${sep}StoreAndForwardDAO.js`,
     `lib${sep}org${sep}chromium${sep}webidl${sep}LocalGitIDLFileDAO.js`,
+
+    // Pipeline components
+    `lib${sep}org${sep}chromium${sep}webidl${sep}URLExtractor.js`,
   ];
 })();
