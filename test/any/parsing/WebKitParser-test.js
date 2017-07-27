01828cd976910e69ca82f3c4a1f73aa4a520ac20
Michael Cheung <michaelcheung@google.com>
2017-07-27 10:40:53 -0400

    Updating the way AST node initializes source info

--- test/any/parsing/WebKitParser-test.js
+++ test/any/parsing/WebKitParser-test.js
@@ -12,7 +12,7 @@ describe('WebKitParser', function() {
   });
 
   function parse(str, opt_production) {
-    var p = WebKitParser.create().parseString(str, null, opt_production);
+    var p = WebKitParser.create().parseString(str, 'Test', opt_production);
     expect(p.pos).toBe(str.length);
     return p;
   }
