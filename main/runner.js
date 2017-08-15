f80abf411ada4e43dda11d839f4f178410f6dc56
Michael Cheung <michaelcheung@google.com>
2017-08-15 10:14:16 -0400

    Potential script for auto-data collection

--- main/runner.js
+++ main/runner.js
@@ -19,8 +19,12 @@ const geckoConfig = require('./geckoConfig.js').config;
 const webKitConfig = require('./webKitConfig.js').config;
 
 // URL Filters
-var include = [/dev\.w3\.org/, /github\.io/, /spec\.whatwg\.org/, /css-houdini\.org/, /csswg\.org/, /svgwg\.org/, /drafts\.fxtf\.org/, /www\.khronos\.org\/(registry\/webgl\/specs\/latest\/[12]\.0|registry\/typedarray\/specs\/latest)/, /www\.w3\.org\/TR\/geolocation-API/, /dvcs\.w3\.org\/hg\/speech-api\/raw-file\/tip\/webspeechapi\.html/];
-var exclude = [/web\.archive\.org/, /archives/, /html\.spec\.whatwg\.org\/multipage/];
+var include = [/dev\.w3\.org/, /github\.io/, /spec\.whatwg\.org/,
+    /css-houdini\.org/, /csswg\.org/, /svgwg\.org/, /drafts\.fxtf\.org/,
+    /www\.khronos\.org\/(registry\/webgl\/specs\/latest\/[12]\.0|registry\/typedarray\/specs\/latest)/,
+    /www\.w3\.org\/TR\/geolocation-API/,
+    /dvcs\.w3\.org\/hg\/speech-api\/raw-file\/tip\/webspeechapi\.html/];
+var exclude = [/web\.archive\.org/, /archives/, /multipage/];
 
 var LocalGitRunner = foam.lookup('org.chromium.webidl.LocalGitRunner');
 var WebPlatformEngine = foam.lookup('org.chromium.webidl.WebPlatformEngine');
@@ -31,56 +35,56 @@ var IDLFragmentExtractorRunner = foam.lookup('org.chromium.webidl.IDLFragmentExt
 var ParserRunner = foam.lookup('org.chromium.webidl.ParserRunner');
 var CanonicalizerRunner = foam.lookup('org.chromium.webidl.CanonicalizerRunner');
 var DiffRunner = foam.lookup('org.chromium.webidl.DiffRunner');
+var JournalingContainer = foam.lookup('org.chromium.webidl.JournalingContainer');
 
-// Preparing pipelines
-var ctx = foam.box.Context.create();
-ctx.registry = foam.box.SelectorRegistry.create({
-  selector: FetchSpecRegistrySelector.create(null, ctx),
-}, ctx);
 var PipelineBuilder = foam.box.pipeline.PipelineBuilder;
 
-// Pipeline Description
-// Fetch from Repositories -> Extract IDL Files -> Process into IDLFileContents
-//
-//           (Blink) 1-> Extract URLs -> Fetch HTML -> Process HTML -> Process IDL Files -> ...
-// ->Branch{
-//           (Other) 2-> To Datastore -> ...
-// -> Put partials together -> Diff -> Back to datastore
+// Preparing general context for Pipelines.
+var ctx = foam.box.Context.create();
+ctx = JournalingContainer.create({
+  outputDir: path.resolve(__dirname, '../data'),
+}, ctx);
 
 // Inject properties into all of the configs.
 var sharedPath = PipelineBuilder.create(null, ctx)
-                                .append(DiffRunner.create());
+  .append(DiffRunner.create(null, ctx));
 
-[ blinkConfig, geckoConfig, webKitConfig ].forEach(function(config) {
+[blinkConfig, geckoConfig, webKitConfig].forEach(function(config) {
+  var localCtx = ctx.__subContext__.createSubContext({source: config.source});
   var corePath = PipelineBuilder.create(null, ctx)
-                                .append(ParserRunner.create({ parserType: config.parserClass }))
-                                .append(CanonicalizerRunner.create({ source: config.source }))
-                                .append(sharedPath);
+      .append(ParserRunner.create({parserType: config.parserClass}, localCtx))
+      .append(CanonicalizerRunner.create({source: config.source}, localCtx))
+      .append(sharedPath);
 
+  // URL fetching pipeline for Blink repository.
   if (config.source === WebPlatformEngine.BLINK) {
-    var blinkPL = PipelineBuilder.create(null, ctx)
-                                .append(FetchSpecRunner.create())
-                                .append(IDLFragmentExtractorRunner.create())
-                                .append(ParserRunner.create())
-                                .append(CanonicalizerRunner.create({ source: WebPlatformEngine.SPECIFICATION }))
-                                .append(sharedPath)
-                                .build();
+    var innerCtx = ctx.__subContext__.createSubContext({
+      source: WebPlatformEngine.SPECIFICATION,
+//      registry: foam.box.SelectorRegistry.create({
+//        selector: FetchSpecRegistrySelector.create(null, ctx),
+//      }, ctx),
+    });
+
+    var blinkPL = PipelineBuilder.create(null, innerCtx)
+        .append(FetchSpecRunner.create(null, innerCtx))
+        .append(IDLFragmentExtractorRunner.create(null, innerCtx))
+        .append(ParserRunner.create(null, innerCtx))
+        .append(CanonicalizerRunner.create({
+          source: WebPlatformEngine.SPECIFICATION,
+        }, innerCtx))
+        .append(sharedPath)
+        .build();
     config.urlOutputBox = blinkPL;
   }
   config.fileOutputBox = corePath.build();
   config.include = include;
   config.exclude = exclude;
-  config.freshRepo = true; // For this purpose...
+  config.freshRepo = false; // For this purpose...
+
+  // Removing properties to prevent warning from FOAM.
   delete config.parserClass;
   delete config.source;
+
+  // Start pipeline.
+  LocalGitRunner.create(config, localCtx).run();
 });
-
-
-// Blink Pipeline
-LocalGitRunner.create(blinkConfig).run();
-
-// Gecko Pipeline
-LocalGitRunner.create(geckoConfig).run();
-
-// WebKit Pipeline
-LocalGitRunner.create(webKitConfig).run();
