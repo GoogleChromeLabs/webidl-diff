e72ea01f2adb53bc45f78ae46b2a2b36b40a626f
Michael Cheung <michaelcheung@google.com>
2017-08-16 10:58:30 -0400

    Adding configuration files for LocalGitRunner

---
+++ main/blinkConfig.js
@@ -0,0 +1,34 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+'use strict';
+
+var IDLFileContents = foam.lookup('org.chromium.webidl.IDLFileContents');
+var GitilesIDLFile = foam.lookup('org.chromium.webidl.GitilesIDLFile');
+var WebPlatformEngine = foam.lookup('org.chromium.webidl.WebPlatformEngine');
+var Parser = foam.lookup('org.chromium.webidl.Parser');
+
+var gitilesBaseURL = 'https://chromium.googlesource.com/chromium/src/+';
+var config = {
+  source: WebPlatformEngine.BLINK,
+  parserClass: Parser,
+  repositoryURL: 'https://chromium.googlesource.com/chromium/src.git',
+  localRepositoryPath: require('path').resolve(__dirname, 'data/blink/git'),
+  sparsePath: 'third_party/WebKit/Source',
+  findExcludePatterns: ['*/testing/*', '*/bindings/tests/*', '*/mojo/*'],
+  extension: 'idl',
+  freshRepo: true, // Forces the latest copy of repo to be fetched.
+};
+config.idlFileContentsFactory = function(path, contents, urls) {
+  return IDLFileContents.create({
+    metadata: GitilesIDLFile.create({
+      repository: this.repositoryURL,
+      gitilesBaseURL: gitilesBaseURL,
+      revision: this.commit,
+      path: path,
+    }),
+    contents: contents,
+    specUrls: urls,
+  });
+};
+module.exports = { config };
---
+++ main/webKitConfig.js
@@ -0,0 +1,34 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+'use strict';
+
+var IDLFileContents = foam.lookup('org.chromium.webidl.IDLFileContents');
+var GithubIDLFile = foam.lookup('org.chromium.webidl.GithubIDLFile');
+var WebPlatformEngine = foam.lookup('org.chromium.webidl.WebPlatformEngine');
+var WebKitParser = foam.lookup('org.chromium.webidl.WebKitParser');
+
+var githubBaseURL = 'https://github.com/WebKit/webkit';
+var config = {
+  source: WebPlatformEngine.WEBKIT,
+  parserClass: WebKitParser,
+  repositoryURL: 'https://github.com/WebKit/webkit.git',
+  localRepositoryPath: require('path').resolve(__dirname, 'data/WebKit/git'),
+  sparsePath: 'Source/WebCore',
+  findExcludePatterns: ['*/testing/*', '*/test/*', '*/deprecated/*'],
+  extension: 'idl',
+  freshRepo: true, // Forces the latest copy of repo to be fetched.
+};
+config.idlFileContentsFactory = function(path, contents) {
+  // Classes are injected by ...
+  return IDLFileContents.create({
+    metadata: GithubIDLFile.create({
+      repository: this.repositoryURL,
+      githubBaseURL: githubBaseURL,
+      revision: this.commit,
+      path: path,
+    }),
+    contents: contents,
+  });
+};
+module.exports = { config };
