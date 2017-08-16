b9f73f1ced5e6afac4a456372b2112bd4a990f75
Michael Cheung <michaelcheung@google.com>
2017-08-16 12:13:21 -0400

    Changing message to include date instead of build number.

--- scripts/collect-and-commit-data.sh
+++ scripts/collect-and-commit-data.sh
@@ -8,7 +8,7 @@ git config --global user.name "Travis CI"
 
 # Synchronize branch with master.
 echo "Synchronizing branch with master."
-git remote set-url origin https://$GIT_TOKEN@github.com/GoogleChrome/webidl-diff.git > /dev/null 2>&1
+git remote set-url origin https://${GIT_TOKEN}@github.com/GoogleChrome/webidl-diff.git > /dev/null 2>&1
 git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
 git fetch
 git checkout dataCollection
@@ -22,7 +22,7 @@ node --max_old_space_size=4096 main/runner.js > /dev/null
 # Upload the files.
 if [ $? -eq 0 ]; then
   git add data/*
-  git commit --message "Automated collection - Travis Build $TRAVIS_BUILD_NUMBER [skip ci]"
+  git commit --message "Automated collection - $(date --utc --iso-8601=seconds) [skip ci]"
   git push --quiet --set-upstream origin dataCollection
   exit 0
 else
