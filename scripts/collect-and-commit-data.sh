a77a31ca83f3b61f628072141145ac61a5f90424
Michael Cheung <michaelcheung@google.com>
2017-08-17 22:30:55 -0400

    Alternative to travis_wait

--- scripts/collect-and-commit-data.sh
+++ scripts/collect-and-commit-data.sh
@@ -17,7 +17,18 @@ git merge origin/master
 # Run Data Collection.
 echo "Beginning data collection."
 mkdir -p data   # Ensure that this directory exists for data output.
-node --max_old_space_size=4096 main/runner.js > /dev/null
+
+node --max_old_space_size=4096 main/runner.js > /dev/null &
+minutes=0
+limit=45 # Timeout after 45 minutes
+while kill -0 $! >/dev/null 2>&1; do
+  echo -n -e " \b"
+  if [ $minutes == $limit ]; then
+    break;
+  fi
+  minutes=$((minutes+1))
+  sleep 60
+done
 
 # Upload the files.
 if [ $? -eq 0 ]; then
