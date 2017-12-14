ef335d3067a62316524a05a18aaae0159316214f
Mark Dittmer <mark.s.dittmer@gmail.com>
2017-12-14 08:23:47 -0500

    WIP: Start building dockerized environments for dev and containerized services

---
+++ docker_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -evh
+
+source $(dirname $0)/env.sh
+
+docker build \
+    --build-arg "GCLOUD_PROJECT_ID=${GCLOUD_PROJECT_ID}" \
+    --build-arg "USER_HOME=${C_USER_HOME}" \
+    --build-arg "USER_ID=${C_USER_ID}" \
+    --build-arg "USER_NAME=${C_USER_NAME}" \
+    --build-arg "GROUP_ID=${C_GROUP_ID}" \
+    --build-arg "GROUP_NAME=${C_GROUP_NAME}" \
+    --build-arg "USER_HOME=${C_USER_HOME}" \
+    -t "${H_IMAGE_TAG}" \
+    -f Dockerfile \
+    .
---
+++ env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export GCLOUD_PROJECT_ID="webidl-diff"
+
+# Development host configuration
+export H_APP_HOME=$(readlink -f $(dirname $0))
+export H_IMAGE_TAG="webidl-diff"
+export H_INSTANCE_NAME="webidl-diff-instance"
+
+# Container configuration
+export C_USER_HOME=${USER_HOME:-/home/webidl-diff}
+export C_USER_ID=1001
+export C_USER_NAME=webidl-diff
+export C_GROUP_ID=1001
+export C_GROUP_NAME=webidl-diff
+export C_USER_HOME="/home/${C_USER_NAME}"
+export C_APP_HOME="${C_USER_HOME}/webidl-diff"
