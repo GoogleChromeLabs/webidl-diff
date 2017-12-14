0738df0e8bc217893af916508f64c311205dc4aa
Mark Dittmer <markdittmer@google.com>
2017-12-14 13:19:39 -0500

    WIP: main/test.js now tests new gcp.StackDriverLogger

--- main/test.js
+++ main/test.js
@@ -4,25 +4,70 @@
 'use strict';
 
 const process = require('process');
-const projectId = process.env.GCLOUD_PROJECT_ID;
-if (!projectId) {
-  throw new Error('Failed to get environment variable GCLOUD_PROJECT_ID');
-  process.exit(1);
-}
-const Logging = require('@google-cloud/logging');
-const logging = new Logging({projectId});
-const logger = logging.log(projectId);
+
 const isProd = process.env.PROD;
-const logContext = isProd ? {
-  prod: true,
-} : {
-  dev: true
-};
+const env = isProd ? 'PROD' : 'DEV';
+const projectId = process.env.GCLOUD_PROJECT_ID;
 
-const __console__ = global.console;
-console.log = global.console.log = function() {
-  const context = Object.assign({}, logContext, {logLevel: 'info'});
-  return logger.write(logger.entry(context, arguments));
-};
+//
+// Override logging globally.
+//
+(() => {
+  if (!projectId) {
+    throw new Error('Failed to get environment variable GCLOUD_PROJECT_ID');
+    process.exit(1);
+  }
+  const Logging = require('@google-cloud/logging');
+  const logging = new Logging({projectId});
+  const logger = logging.log(projectId);
+  const logContext = isProd ? {
+    labels: {
+      project_id: projectId,
+      env,
+    },
+  } : {
+    labels: {
+      project_id: projectId,
+      env,
+    },
+  };
+  const severities = {
+    debug: 'DEBUG',
+    log: 'INFO',
+    info: 'INFO',
+    warn: 'WARNING',
+    error: 'ERROR',
+  };
+  const bindLogFunction = name => {
+    console[name] = global.console[name] = function() {
+      const metadata = Object.assign({}, logContext, {
+        severity: severities[name],
+      });
+      let data;
+      if (arguments.length === 1) {
+        data = arguments[0];
+      } else {
+        data = {};
+        for (let i = 0; i < arguments.length; i++) {
+          data[i] = arguments[i];
+        }
+      }
+      logger.write(logger.entry(metadata, data));
+    };
+  };
+  bindLogFunction('debug');
+  bindLogFunction('log');
+  bindLogFunction('info');
+  bindLogFunction('warn');
+  bindLogFunction('error');
+})();
 
-console.log('hello', true, {foo: null});
+require('../lib/org/chromium/webidl/gcp/StackDriverLogger.js');
+
+const logger = org.chromium.webidl.gcp.StackDriverLogger.create(null,
+    foam.createSubContext({
+      gcloudProjectId: projectId,
+      gcloudEnv: env,
+    }));
+
+logger.log('Hello', 'there');
