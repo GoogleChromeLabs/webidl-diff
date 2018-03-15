71954d82cf13f1d39ef389d012a4cf48a4609cfb
Luke Bjerring <lukebjerring@users.noreply.github.com>
2018-03-15 10:57:05 -0400

    Trim common whitespace per snippet

--- test/node/update-wpt-test.js
+++ test/node/update-wpt-test.js
@@ -3,7 +3,7 @@
 const path = require('path')
 const updateWPT = require(path.resolve(__dirname, '..', '..', 'main', 'update-wpt.js'));
 
-describe('trimCommonLeadingSpaces', function () {
+fdescribe('trimCommonLeadingSpaces', function () {
   it(`shouldn't trim trailing spaces`, function () {
     var idl =
 `  interface MyInterface {  
@@ -34,6 +34,26 @@ describe('trimCommonLeadingSpaces', function () {
 interface MyInterface {
   boolean testPasses();
 }`;
+
+    let trimmed = updateWPT.trimCommonLeadingSpaces(idl);
+    expect(trimmed).toEqual(expected);
+  });
+
+  it(`should ignore whitespace-only lines`, function () {
+    var idl = `
+  
+    interface MyInterface {
+      boolean testPasses();
+    }
+ 
+    partial interface MyOtherInterface {}`;
+    var expected = `
+
+interface MyInterface {
+  boolean testPasses();
+}
+
+partial interface MyOtherInterface {}`;
 
     let trimmed = updateWPT.trimCommonLeadingSpaces(idl);
     expect(trimmed).toEqual(expected);
